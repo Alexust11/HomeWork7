bd115ab5438bfa354ed127368e04418d8dfd3f76
Устюгов <alexust11@yandex.ru>
2022-08-18 18:12:27 +0700

    Решены задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Задача 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+//        Задача 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+//        Задача 3
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
